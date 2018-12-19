73b9ec90f6a0e9c4fb5c2c1fb3340a02f5be433d
cnbigjx <gjx@cnbisoft.com>
2018-12-19 20:07:47 +0800

    修复修改期间表格数据不变的问题

--- src/components/table/BiTableColumnTree.vue
+++ src/components/table/BiTableColumnTree.vue
@@ -2,10 +2,10 @@
     <el-table-column  v-if="isFolder" :key="col.text" :label="col.text" :prop="col.id" >
         <template v-for="cc in col.children">
             <bi-table-column-tree v-if="cc.children && cc.children.length > 0 "  :col="cc"  :key="cc.text" :data="data"></bi-table-column-tree>
-            <bi-table-column v-else :col="cc" :data="data" :key="cc.text" />
+            <bi-table-column v-else :col="cc" :data="data" :key="cc.text" ref="tcol"/>
         </template>
     </el-table-column>
-   <bi-table-column v-else    :col="col"  :key="col.text" :data="data" />
+   <bi-table-column v-else    :col="col"  :key="col.text" :data="data" ref="tcol"/>
 </template>
 
 <script>
@@ -25,7 +25,15 @@ export default {
       upData(item){
           this.$set(this,"data",item);
           this.$set(this.data,"datas",item.datas);
-      },
+          let refs = this.$refs;
+          if(refs){
+                if(refs.tcol){
+                   if(refs.tcol.upData){
+                      refs.tcol.upData(item);
+                   }
+            }
+        }
+      }      
   }
 }
 </script>
--- src/utils/modeHandle.js
+++ src/utils/modeHandle.js
@@ -26,8 +26,10 @@ export default function modeHandle(e) {
         .then(response => {
             let bean = response.data;
             if (bean.data && bean.data.source) {
+                //数据库来的
                 bean = bean.data.source || 0;
-            } else {
+            } else if(bean.id) {
+                //加处理函数来的
                 bean = JSON.stringify(bean);
             }
             //  debugger;
