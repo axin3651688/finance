3fc60c4ed93098d2e29175e4eb089f283da9e719
马军 <mj@cnbisoft.com>
2018-12-21 09:10:27 +0800

    pie修改

--- src/api/socketCoreProcess.js
+++ src/api/socketCoreProcess.js
@@ -10,7 +10,9 @@ import {
     isArray
 } from 'util';
 import cmd1500Handle from './cmd1500Handle'
-import {processSingleMessage} from 'utils/message'
+import {
+    processSingleMessage
+} from 'utils/message'
 /**
  * 消息核心处理
  * author:gjx
@@ -55,49 +57,49 @@ export default function socketCoreProcess(websocket, datas) {
 
     let allowNotification = window.Notification;
 
-    let showNotification=function(data){
+    let showNotification = function (data) {
         let bean = data.data;
-        let user  = bean.user;
-        debugger;
+        let user = bean.user;
+        // debugger;
         let who = "自己";
-        if(user){
+        if (user) {
             who = "别人";
-          }
-        var title = title  = "收到" + who+ "的消息" ;
+        }
+        var title = title = "收到" + who + "的消息";
         $notify.success({
             title: title,
-            message: data.msg ,
+            message: data.msg,
             showClose: true,
-            position:"bottom-right"
-          });
-    //    var n = new allowNotification(title, {// 标题
-    //        body : "内容：" +data.msg , // 显示内容
-    //        icon : user?user.avatar:"http://jiaxin365.cn/images/cloud/biimg/daiban_iconweb.png",
-    //        lang : 'zh-CN',
-    //        data : data
-    //    });
-    //    n.onclick = function(event,msg){
-    //        event.preventDefault(); // prevent the browser from focusing the
-    //        window.focus();
-    //        //Cnbi.Msg.alert("可传入回调函数执行哦，亲！",4);
-    //        // var $btn = $("#layui-layim-min").parent();// $btn.trigger("click");
-    //        n.close();
-    //    }
-    //    setTimeout(function() {n.close();}, 6000);// 两秒后关闭通知
+            position: "bottom-right"
+        });
+        //    var n = new allowNotification(title, {// 标题
+        //        body : "内容：" +data.msg , // 显示内容
+        //        icon : user?user.avatar:"http://jiaxin365.cn/images/cloud/biimg/daiban_iconweb.png",
+        //        lang : 'zh-CN',
+        //        data : data
+        //    });
+        //    n.onclick = function(event,msg){
+        //        event.preventDefault(); // prevent the browser from focusing the
+        //        window.focus();
+        //        //Cnbi.Msg.alert("可传入回调函数执行哦，亲！",4);
+        //        // var $btn = $("#layui-layim-min").parent();// $btn.trigger("click");
+        //        n.close();
+        //    }
+        //    setTimeout(function() {n.close();}, 6000);// 两秒后关闭通知
 
     }
 
-     let permission = allowNotification.permission;
-        if (permission == "denied") {
-            debugger;
-            var notification = new window.Notification("这是一个通知撒:", {  
-                dir: "auto",  
-                lang: "hi",  
-                tag: "testTag",  
-                icon: "https://static.cnblogs.com/images/adminlogo.gif",  
-                body: "通知content"  
-            });  
-        }
+    let permission = allowNotification.permission;
+    if (permission == "denied") {
+        debugger;
+        var notification = new window.Notification("这是一个通知撒:", {
+            dir: "auto",
+            lang: "hi",
+            tag: "testTag",
+            icon: "https://static.cnblogs.com/images/adminlogo.gif",
+            body: "通知content"
+        });
+    }
 
 
     // debugger;
