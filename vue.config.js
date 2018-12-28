cbccb76cc5bce2bf9519059c375040f6759dcad2
cnbisjz <sjz@cnbisoft.com>
2018-12-28 19:08:17 +0800

    sjz提交json文件

--- vue.config.js
+++ vue.config.js
@@ -39,8 +39,8 @@ let proxyObj = { // 配置跨域
         }
     },
     '/cnbi': {
-         target: 'http://192.168.1.118:8081', //gjx
-        //target: 'http://192.168.1.149:8000',
+        //  target: 'http://192.168.1.118:8081', //gjx
+        target: 'http://192.168.2.34:8081',
 
         ws: true,
         changOrigin: true,
