4dca261429574133820ff20a6428ad6915fa2358
马军 <mj@cnbisoft.com>
2018-12-28 10:05:27 +0800

    11

--- src/components/BiTable.vue
+++ src/components/BiTable.vue
@@ -1,18 +1,19 @@
 <template>
-<div>
-  <el-table
-    :data.sync="(item.config.rows && item.config.rows.length > 0)?item.config.rows : item.datas"
-    border
-    :stripe="true"
-    height="item.height || rowClass"
-    :cell-style="cellStyle"
-    @cell-click="onCellClick"
-     :span-method="rowSpanAndColSpanHandler" 
-  >
-    <el-tag v-for="cc in item.config.columns" v-bind:key="cc.id" v-if="!cc.hidden">
-      <bi-table-column-tree :col="cc" :tableData.sync="item" ref="tchild"/>
-    </el-tag>
-  </el-table>
+  <div>
+    <el-table
+      :data.sync="(item.config.rows && item.config.rows.length > 0)?item.config.rows : item.datas"
+      border
+      :stripe="true"
+      height="item.height || rowClass"
+      :cell-style="cellStyle"
+      @cell-click="onCellClick"
+      :span-method="rowSpanAndColSpanHandler"
+      :header-cell-style="{'background':item.class_bg ? item.class_bg:'#F0F8FF'}"
+    >
+      <el-tag v-for="cc in item.config.columns" v-bind:key="cc.id" v-if="!cc.hidden">
+        <bi-table-column-tree :col="cc" :tableData.sync="item" ref="tchild"/>
+      </el-tag>
+    </el-table>
     <!-- sjz 分页功能 -->
     <el-pagination
       v-if="item.pagination"
@@ -42,8 +43,8 @@ export default {
   data() {
     return {
       dialogVisible: false,
-      currentPage:1,
-      pagesize:1,
+      currentPage: 1,
+      pagesize: 1,
       id: 0,
       text: "",
       rows: [],
@@ -51,15 +52,15 @@ export default {
       columns: [],
       groupConfig: {
         idProperty: "group",
-        textProperty: "groupName",
+        textProperty: "groupName"
       },
-      drillProperties:["text","text_"],//有钻取，给蓝色
-      levelProperties:{text:"level",text_:"level_"}//加缩进
+      drillProperties: ["text", "text_"], //有钻取，给蓝色
+      levelProperties: { text: "level", text_: "level_" } //加缩进
     };
   },
 
   created() {
-    debugger
+    debugger;
     this.upData(this.item);
     //debugger;
     //this.getTableDataParams();
@@ -75,12 +76,12 @@ export default {
 
   methods: {
     handleClose(done) {
-        this.$confirm('确认关闭？')
-          .then(_ => {
-            done();
-          })
-          .catch(_ => {});
-      },
+      this.$confirm("确认关闭？")
+        .then(_ => {
+          done();
+        })
+        .catch(_ => {});
+    },
     //pagesize改变时触发 ---- 分页功能
     handleSizeChange: function(size) {
       this.pagesize = size;
@@ -137,7 +138,7 @@ export default {
      */
     cellStyle(row) {
       if (this.item.cellStyle && typeof this.item.cellStyle == "function") {
-        return this.item.cellStyle(row,this);
+        return this.item.cellStyle(row, this);
       }
       let css = "padding: 4px 0;";
       let pro = row.column.property;
@@ -145,11 +146,13 @@ export default {
         return css;
       }
       let levelProperties = this.item.levelProperties || this.levelProperties;
-      let textIndent ="",record = row.row;
+      let textIndent = "",
+        record = row.row;
       let levelPro = levelProperties[pro];
       if (levelPro && record[levelPro]) {
-          let level = record[levelPro] || 1;
-          textIndent = level > 1 ? "text-indent: " + (level - 1) * 20 + "px;" : ";";
+        let level = record[levelPro] || 1;
+        textIndent =
+          level > 1 ? "text-indent: " + (level - 1) * 20 + "px;" : ";";
       }
       let drillProperties = this.item.drillProperties || this.drillProperties;
       if (drillProperties.indexOf(pro) != -1) {
@@ -157,12 +160,12 @@ export default {
         css = css + "font-weight:bold;" + textIndent + drill;
         return css;
       } else {
-        return css+textIndent;
+        return css + textIndent;
       }
     },
-     /**
-      * 单元格单击默认事件
-      */
+    /**
+     * 单元格单击默认事件
+     */
     onCellClickDefault(row, column, cell, event) {
       let listener = row._drill || row.drill;
       if (listener) {
@@ -186,10 +189,10 @@ export default {
      * 单元格单击事件
      */
     onCellClick(row, column, cell, event) {
-       if(this.item.onCellClick && typeof(this.item.onCellClick) == "function"){
-            return this.item.onCellClick(row, column, cell, event,this);
-        }
-        this.onCellClickDefault(row, column, cell, event);
+      if (this.item.onCellClick && typeof this.item.onCellClick == "function") {
+        return this.item.onCellClick(row, column, cell, event, this);
+      }
+      this.onCellClickDefault(row, column, cell, event);
       // this.dialogVisible = true
       // this.a = event.path[0].innerHTML //获取到某一个单元格数据
       // this.b = event.target.innerHTML//获取到某一个单元格数据
@@ -202,11 +205,12 @@ export default {
     },
 
     /**
-     * 获取rowspan   
+     * 获取rowspan
      */
     getCellRowSpan(datas, row, config) {
-      return datas.filter(record => record[config.idProperty] === row[config.idProperty])
-        .length;
+      return datas.filter(
+        record => record[config.idProperty] === row[config.idProperty]
+      ).length;
     },
     /**
      * 计算每一个单元格的rowspan和colspan
@@ -265,7 +269,7 @@ export default {
         this.item.rowSpanAndColSpanHandler &&
         typeof this.item.rowSpanAndColSpanHandler == "function"
       ) {
-        let cells =  this.item.rowSpanAndColSpanHandler(
+        let cells = this.item.rowSpanAndColSpanHandler(
           { row, column, rowIndex, columnIndex },
           this
         );
@@ -395,10 +399,9 @@ export default {
   height: 0;
 }
 /* 数字靠右 */
-.el-table td.is-center{
+.el-table td.is-center {
   text-align: right;
 }
-.el-table td{
-
+.el-table td {
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -20,7 +20,7 @@ function resolve(dir) {
 }
 let proxyObj = { // 配置跨域
     '/api': {
-       // target: 'http://192.168.1.118:8005',
+        // target: 'http://192.168.1.118:8005',
         //  target: 'http://192.168.1.139:8005',
         // target: 'http://192.168.1.139:8005',
         target: 'http://192.168.2.237:8005',
@@ -39,8 +39,9 @@ let proxyObj = { // 配置跨域
         }
     },
     '/cnbi': {
-        // target: 'http://192.168.1.118:8081', //gjx
-        target: 'http://192.168.2.12:8081',
+        target: 'http://192.168.1.118:8081', //gjx
+        // target: 'http://192.168.2.12:8081',
+        // target: 'http://192.168.1.138:8081',
         ws: true,
         changOrigin: true,
         pathRewrite: {
@@ -90,8 +91,8 @@ module.exports = {
     pages: {
         index: {
             // 页面的入口文件
-           // entry: 'src/views/message_page/main.js',
-             entry: 'src/views/layout/main.js',
+            // entry: 'src/views/message_page/main.js',
+            entry: 'src/views/layout/main.js',
             // 页面的模板文件
             template: 'public/index.html',
             // build 生成的文件名称  例： dist/index.html
