59e9c116d5c14e07a834698670446d812b9be93a
463335042 <szc@cnbisoft.com>
2019-03-13 20:27:31 +0800

    提交

---
+++ src/components/SBiDiv.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+    <div v-html="divContent" class="div_content">
+    </div>
+</div>
+</template>
+<script>
+export default {
+    name:"bidiv",
+    props: [ "dataDiv" ],
+    data() {
+        return {
+            divContent:""
+        };
+    },
+    created() {
+        debugger;
+        this.update();
+    },
+    watch: {},
+    methods: {
+        update () {
+            debugger;
+            if(this.dataDiv){
+                this.divContent = this.dataDiv.htmlContent;
+            }
+        }
+    }
+};
+</script>
+<style lang='scss' scoped>
+.div_content {
+    text-align: center;
+    font-weight: bold;
+    /* height: 300px; */
+    line-height: 60px;
+    font-size: 20px;
+    background-color: #ddd;
+}
+</style>
+
