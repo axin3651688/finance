9574b547835f51b25fe824ae8ae158865b62c0f1
sjz <sjz@cnbisoft.com>
2019-03-22 11:03:15 +0800

    提交

--- src/views/systemSettings/dimension.vue
+++ src/views/systemSettings/dimension.vue
@@ -5,7 +5,9 @@
             <el-tab-pane label="风险类型管理" name="first" >风险类型管理</el-tab-pane>
             <el-tab-pane label="上市公司管理" name="second">上市公司管理</el-tab-pane>
             <el-tab-pane label="人才信息管理" name="three">人才信息管理</el-tab-pane>
-            <el-tab-pane label="行业板块管理" name="four">行业板块管理</el-tab-pane>
+            <el-tab-pane label="行业板块管理" name="four">
+                <tab-industry></tab-industry>
+            </el-tab-pane>
             <el-tab-pane label="图片编辑管理" name="five">
                 <tab-picture></tab-picture>
             </el-tab-pane>
@@ -14,10 +16,12 @@
 </template>
 <script>
 import tabPicture from "@v/systemSettings/tabPicture"; 
+import tabIndustry from "@v/systemSettings/tabIndustry"; 
 export default {
     name: "dimension",
     components: {
-        tabPicture
+        tabPicture,
+        tabIndustry
     },
     data(){
         return {
--- src/views/systemSettings/tabPicture.vue
+++ src/views/systemSettings/tabPicture.vue
@@ -1,6 +1,10 @@
 <template>
     <div :style="styles">
-        <!-- 维度管理——>图片编辑管理 -->
+        <!-- 维度管理——>图片编辑管理(新) -->
+        <!-- 
+            name : sjz
+            time : 2019/3/21 
+         -->
         <!-- 
          
             响应式布局：
@@ -13,82 +17,34 @@
         -->
         <el-row :gutter="10">
             <el-col :xs="12" :sm="24" :lg="24">
-                <div class="pic">
-                    <div class="pic_first" :style="styler">
-                        <div class="text">
-                            <span>轮播图一</span>
-                        </div>
-                        <div class="picture">
-                            <img src="@a/loginT/first.jpg">
-                        </div>
-                        <div class="buttons">
-                            <el-button type="primary" disabled>替换文件</el-button>
-                            <span>默认图片，不可替换</span>
-                            <!-- <el-upload
-                                class="avatar-uploader"
-                                action="https://jsonplaceholder.typicode.com/posts/"
-                                :show-file-list="false"
-                                :on-success="handleAvatarSuccess"
-                                :before-upload="beforeAvatarUpload">
-                                <img src="@a/loginT/first.jpg" class="avatar">
-                                <el-button type="primary" @click="pic_click(this)" disabled>替换文件</el-button>
-                                <span>默认图片，不可替换</span>
-                            </el-upload> -->
-                        </div>
+                <div class="pic_first" :style="styler">
+                    <div class="text">
+                        <span>轮播图一</span>
                     </div>
-                    <div class="pic_second" :style="stylex">
-                        <div class="text">
-                            <span>轮播图二</span>
-                        </div>
-                        <div class="button">
-                            <el-upload
-                                class="avatar-uploader"
-                                action="/zjb/upload/carousel?id=2"
-                                :show-file-list="false"
-                                :on-success="handleAvatarSuccess"
-                                :before-upload="beforeAvatarUpload">
-                                <img v-if="imageUrl2" :src="imageUrl2" class="avatar">
-                                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-                                <el-button type="primary" >替换文件</el-button>
-                                <span>支持jpg、jpeg、png文件</span>
-                            </el-upload>
-                        </div>
+                    <div class="picture">
+                        <img src="@a/loginT/first.jpg">
                     </div>
-                    <div class="pic_three" :style="stylex">
-                        <div class="text">
-                            <span>轮播图三</span>
-                        </div>
-                        <div class="button">
-                            <el-upload
-                                class="avatar-uploader"
-                                action="/zjb/upload/carousel?id=3"
-                                :show-file-list="false"
-                                :on-success="handleAvatarSuccess"
-                                :before-upload="beforeAvatarUpload">
-                                <img v-if="imageUrl3" :src="imageUrl3" class="avatar">
-                                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-                                <el-button type="primary" >替换文件</el-button>
-                                <span>支持jpg、jpeg、png文件</span>
-                            </el-upload>
-                        </div>
+                    <div class="buttons">
+                        <el-button type="primary" disabled>替换文件</el-button>
+                        <span>默认图片，不可替换</span>
                     </div>
-                    <div class="pic_four" :style="stylex">
-                        <div class="text">
-                            <span>轮播图四</span>
-                        </div>
-                        <div class="button">
-                            <el-upload
-                                class="avatar-uploader"
-                                action="/zjb/upload/carousel?id=4"
-                                :show-file-list="false"
-                                :on-success="handleAvatarSuccess"
-                                :before-upload="beforeAvatarUpload">
-                                <img v-if="imageUrl4" :src="imageUrl4" class="avatar">
-                                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-                                <el-button type="primary">替换文件</el-button>
-                                <span>支持jpg、jpeg、png文件</span>
-                            </el-upload>
-                        </div>
+                </div>
+                <div :style="stylex" v-for="item in items">
+                    <div class="text">
+                        <span>{{ item.text }}</span>
+                    </div>
+                    <div class="button">
+                        <el-upload                            
+                            class="avatar-uploader"
+                            :action="item.action"
+                            :show-file-list="false"
+                            :on-success="handleAvatarSuccess"
+                            :before-upload="beforeAvatarUpload">
+                            <img v-if="item.imageUrl" :src="item.imageUrl" class="avatar">
+                            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                            <el-button type="primary" >替换文件</el-button>
+                            <span>支持jpg、jpeg、png文件</span>
+                        </el-upload>
                     </div>
                 </div>
             </el-col>
@@ -98,9 +54,13 @@
 <script>
 import { UploadShow } from '~api/cube';
 export default {
-    name: "tabPicture",
     data(){
         return {
+            items: [
+                { id: '2', text: '轮播图二', action: '/zjb/upload/carousel?id=2', imageUrl: '' },
+                { id: '3', text: '轮播图三', action: '/zjb/upload/carousel?id=3', imageUrl: '' },
+                { id: '4', text: '轮播图四', action: '/zjb/upload/carousel?id=4', imageUrl: '' }
+            ],
             styles: {
                 height: "0px",
                 backgroundColor: "#fff"
@@ -118,48 +78,19 @@ export default {
                 boxShadow: "0px 0px 10px #ccc",
                 float : "left",
                 marginTop : "20px"
-            },
-            
-            imageUrl2 : '' ,    // 轮播图二
-            imageUrl3 : '' ,    // 轮播图三
-            imageUrl4 : ''      // 轮播图四
+            }
         }
     },
     created(){
-        // debugger
         this.styles.height = document.body.offsetHeight - 40 - 64 - 32 + "px" ;
     },
     mounted(){
         // （1）自适应高度
         this.setTableScollHeight() ;
         // （3）初始化轮播图
-        this.initCarousels();
+        this.initCarousels() ;
     },
     methods: {
-        
-        // （3）初始化轮播图 防止刷新页面的时候图片消失
-        initCarousels(){
-            // 走马灯轮播请求
-            UploadShow().then(ress => {
-                if(ress.data.code == 200){
-                    let datas = ress.data.data;
-                    if(datas.length > 0 ){
-                        [2,3,4].forEach(element => {
-                            this.setCarousel(datas,element);
-                        });
-                        
-                    }
-                }
-            })
-        },
-        // （3.1）轮播图循环赋值
-        setCarousel(datas,key){
-            let d = datas.filter(ele=>{
-                return (ele.name - 0) === (key - 0)
-            })
-            debugger
-            this["imageUrl" + key] = d && d.length == 1 ? d[0].content  : '';
-        },
         // （1）自适应高度方法
         setTableScollHeight(res){
             // debugger
@@ -191,16 +122,39 @@ export default {
         // （2.1）上传图片成功的方法
         handleAvatarSuccess(res, file, fileList){
             if(res.data.code == 200){
-                this["imageUrl"+ res.data.data.name] = res.data.data.content + "?t=" + new Date().getTime();
+                this.items.forEach(vax => {
+                    if(vax.id === res.data.data.name){
+                        vax.imageUrl = res.data.data.content + "?t=" + new Date().getTime();
+                    }
+                })
             }else{
                 this.$message.error('图片上传失败了!');
             }
+        },
+        // （3）初始化轮播图 防止刷新页面的时候图片消失
+        initCarousels(){
+            // 走马灯轮播请求
+            UploadShow().then(ress => {
+                let me = this ;
+                if(ress.data.code == 200){
+                    let datas = ress.data.data;
+                    if(datas.length > 0 ){
+                        me.items.forEach(element => {
+                            datas.forEach(item => {
+                                if(element.id === item.name){
+                                    element.imageUrl = item.content ;
+                                }
+                            });
+                        });
+                        
+                    }
+                }
+            })
         }
     }
 }
 </script>
 <style scoped>
-    
     .text {
         font-weight: bold ;
         text-align: center;
