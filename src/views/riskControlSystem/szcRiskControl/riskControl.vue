85020eb941a965f36ebe68ca315c69bd596e02a9
szc <szc@cnbisoft.com>
2019-05-06 13:35:25 +0800

    提交

---
+++ src/views/riskControlSystem/szcRiskControl/riskControl.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div>
+            <el-tabs type="border-card">
+                <el-tab-pane label="用户管理">用户管理</el-tab-pane>
+                <el-tab-pane label="配置管理">配置管理</el-tab-pane>
+                <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+                <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+            </el-tabs>
+        </div>
+        <div></div>
+    </div>
+</template>
+<script>
+
+export default {
+    name: "riskControl",
+    data() {
+        return {
+            tabData:[
+                {
+                    
+                },
+            ]
+        }
+    },
+    created() {},
+    mounted () {},
+    methods: {}
+};
+</script>
+<style>
+
+</style>
