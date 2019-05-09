e16d32e1e51678486376f1ecc052e5648fe625ac
doingSomethingGood <978217648@qq.com>
2019-05-09 16:17:51 +0800

    报告界面、公用组件开发

---
+++ src/views/riskControlSystem/publicRiskControl/riskReportComponents/reportInstruction.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <div class="top-form-contents" style="margin-top: 30px">
+            <span>领导批示</span>
+            <div class="top-form-contents" style="margin-bottom: 0">
+                <p style="min-width: 98px;width: 150px;margin-left: 30px">风险策略为：</p>
+                <el-input v-model="this.countermeasures"></el-input>
+            </div>
+        </div>
+        <div class="top-form-contents">
+            <span style="min-width: 194px;width: 194px"></span>
+            <el-input type="textarea" :rows="3" v-model="this.instruction"></el-input>
+        </div>
+
+        <div class="top-form-contents">
+            <span style="min-width: 194px;width: 194px">风险反馈</span>
+            <el-input type="textarea" :rows="4" v-model="this.riskSuggest" placeholder="this.riskSuggest"></el-input>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "reportInstruction",
+        components: {},
+        props: {},
+        data() {
+            return {
+                countermeasures:'应对策略自动显示，不可编辑',
+                instruction:'显示批示内容，不可编辑',
+                riskSuggest:'针对风险进行相关反馈录入'
+            }
+        },
+        created() {
+        },
+        mounted() {
+        },
+        methods: {}
+    }
+</script>
+
+<style scoped>
+    .top-form-contents {
+        display: flex;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+
+    .top-form-contents span {
+        font-size: 16px;
+        color: #333333;
+        font-weight: 700;
+    }
+</style>
---
+++ src/views/riskControlSystem/publicRiskControl/showPersonnelList.vue
@@ -0,0 +1,142 @@
+<template>
+    <div>
+        <div class="personnel-list" v-show="personnelListShow">
+            <el-input
+                    placeholder="输入关键字进行过滤"
+                    v-model="filterText">
+            </el-input>
+            <el-tree
+                    class="filter-tree"
+                    :data="personnelList"
+                    show-checkbox
+                    node-key="id"
+                    default-expand-all
+                    :filter-node-method="filterNode"
+                    ref="tree"
+                    :props="defaultProps">
+            </el-tree>
+            <div class="btn-sure">
+                <el-button type="primary" @click="checkedSure">确定</el-button>
+                <el-button @click="resetChecked">重置</el-button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "showPersonnelList",
+        components: {},
+        props: {
+            personnelListShow: Boolean
+        },
+        data() {
+            return {
+                personnelList: [
+                    {
+                        id: 1,
+                        label: '部门A',
+                        children: [
+                            {
+                                id: 3,
+                                label: '人员A-1'
+                            },
+                            {
+                                id: 4,
+                                label: '人员A-2'
+                            },
+                            {
+                                id: 5,
+                                label: '人员A-3'
+                            },
+                            {
+                                id: 6,
+                                label: '人员A-4'
+                            }
+                        ]
+                    },
+                    {
+                        id: 2,
+                        label: '部门B',
+                        children: [
+                            {
+                                id: 7,
+                                label: '人员B-1'
+                            },
+                            {
+                                id: 8,
+                                label: '人员B-2'
+                            },
+                            {
+                                id: 9,
+                                label: '人员B-3'
+                            },
+                            {
+                                id: 10,
+                                label: '人员B-4'
+                            }
+                        ]
+                    }
+                ],
+                defaultProps: {
+                    children: 'children',
+                    label: 'label'
+                },
+
+                filterText: ''
+            }
+        },
+        created() {
+        },
+        mounted() {
+        },
+        watch: {
+            filterText(val) {
+                this.$refs.tree.filter(val);
+            }
+        },
+        methods: {
+
+            /**
+             * 指定人员搜索
+             * @param value
+             * @param data
+             * @returns {boolean}
+             */
+            filterNode(value, data) {
+                if (!value) return true;
+                return data.label.indexOf(value) !== -1;
+            },
+            resetChecked() {
+                this.$refs.tree.setCheckedKeys([]);
+            },
+            checkedSure() {
+                alert("反馈成功");
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .personnel-list {
+        background: white;
+        width: 200px;
+        margin-top: 10px;
+        border: 1px solid #606266;;
+        padding: 5px;
+        position: absolute;
+        bottom: 100px;
+        right: 20px;
+        height: 354px;
+    }
+
+    .btn-sure {
+        text-align: right;
+        margin-top: 10px;
+    }
+
+    .btn-sure button {
+        border-radius: 16px;
+        padding: 8px 16px;
+    }
+</style>
