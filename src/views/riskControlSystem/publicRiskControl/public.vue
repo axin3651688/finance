7966773947ed1cce1edf3e77002e4185521baa80
sjz <sjz@cnbisoft.com>
2019-05-10 18:00:46 +0800

    qwe4

--- src/views/riskControlSystem/publicRiskControl/public.vue
+++ src/views/riskControlSystem/publicRiskControl/public.vue
@@ -5,10 +5,10 @@
         -->
         <el-tabs v-model="activeName" @tab-click="handleClick">
             <el-tab-pane label="风险追踪" name="first">
-                <ai-module :jsonAdress="jsonAdress2" :tableHeight="tableHeight"></ai-module>       
+                <ai-module v-if="isShow" :jsonAdress="jsonAdress2" :tableHeight="tableHeight2"></ai-module>       
             </el-tab-pane>
             <el-tab-pane label="风险报告追踪" name="second">
-                <ai-module :jsonAdress="jsonAdress" :tableHeight="tableHeight"></ai-module>       
+                <ai-module v-if="!isShow" :jsonAdress="jsonAdress" :tableHeight="tableHeight"></ai-module>       
             </el-tab-pane>
         </el-tabs>
     </div>
@@ -26,18 +26,21 @@ export default {
             jsonAdress: "cnbi/risk/riskTreePublic.json",
             tableHeight: 175,
             //---------------------------
-            jsonAdress2: "cnbi/risk/riskTablePublic.json"
-            // tableHeight2:135
+            jsonAdress2: "cnbi/risk/riskTablePublic.json",
+            tableHeight2:120,
+            //---------------------------
+            isShow: false
         }
     },
     created() {
-
+        // debugger
+        this.isShow = !this.isShow ;
     },
     mounted () {},
     methods: {
         handleClick(a, b){
             // debugger
-
+            this.isShow = !this.isShow ;            
         }
     }
 };
