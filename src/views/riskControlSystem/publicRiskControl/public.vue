57915259c5002a67ef140ce7b7ab6ff088263aa6
doingSomethingGood <978217648@qq.com>
2019-05-13 11:38:57 +0800

    tijiao

--- src/views/riskControlSystem/publicRiskControl/public.vue
+++ src/views/riskControlSystem/publicRiskControl/public.vue
@@ -1,49 +1,19 @@
-<template><!--
-    <div>
-        &lt;!&ndash;
-            风险追踪
-        &ndash;&gt;
-        <el-tabs v-model="activeName" @tab-click="handleClick">
-            <el-tab-pane label="风险追踪" name="first">
-                <ai-module v-if="isShow" :jsonAdress="jsonAdress2" :tableHeight="tableHeight2"></ai-module>       
-            </el-tab-pane>
-            <el-tab-pane label="风险报告追踪" name="second">
-                <ai-module v-if="!isShow" :jsonAdress="jsonAdress" :tableHeight="tableHeight"></ai-module>       
-            </el-tab-pane>
-        </el-tabs>
-    </div>
---></template>>
+<template>
+
+</template>>
 <script>
-// import aiModule from "@v/riskControlSystem/publicRiskControl/AiModule";
-export default {
-    name: "treeTableDemo",
-    components: {
-        // aiModule
-    },
-    data() {
-        return {
-            // activeName: "first",
-            // jsonAdress: "cnbi/risk/riskTreePublic.json",
-            // tableHeight: 175,
-            // //---------------------------
-            // jsonAdress2: "cnbi/risk/riskTablePublic.json",
-            // tableHeight2:120,
-            // //---------------------------
-            // isShow: false
-        }
-    },
-    created() {
-        // debugger
-        // this.isShow = !this.isShow ;
-    },
-    mounted () {},
-    methods: {
-        // handleClick(a, b){
-        //     // debugger
-        //     this.isShow = !this.isShow ;
-        // }
-    }
-};
+    export default {
+        name: "treeTableDemo",
+        components: {},
+        data() {
+            return {}
+        },
+        created() {
+        },
+        mounted() {
+        },
+        methods: {}
+    };
 </script>
 <style>
 
