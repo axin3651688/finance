292ef611f3f92f64fb46186dd5bf033f57c79c81
szc <szc@cnbisoft.com>
2019-05-24 09:25:32 +0800

    提交

--- src/views/riskControlSystem/szcRiskControl/dialogModal/basicsModal.vue
+++ src/views/riskControlSystem/szcRiskControl/dialogModal/basicsModal.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div>
+        <div class="basics_modal">
             <el-form :model="formConfig" label-width="100px">
                 <template v-for="(item,index) in formConfig.groups">
                     <el-row :key="index">
@@ -26,10 +26,11 @@
                                 </el-form-item>
                             </el-col>
                             <el-col v-else-if="it.type == 'labelSelect'" :span="it.span || 12" :key="itIndex">
-                                <el-form-item :label="it.label || '没有配置'" :key="itIndex">
+                                <el-form-item :key="itIndex" class="label_select_content">
+                                    <label slot="label" class="custom_label">{{ it.label }}</label>
                                     <template v-for="(selItem,selIndex) in it.selectConfig">
                                         <label :key="selIndex">{{ selItem.label }}</label>
-                                        <el-select v-model="selectValue" placeholder="请选择" :key="selIndex+10">
+                                        <el-select v-model="selectValues" placeholder="请选择" multiple :key="selIndex+10" :disabled="it.disabled || selectDsiabled">
                                             <el-option
                                             v-for="sltItem in selItem.options"
                                             :key="sltItem.nid"
@@ -39,23 +40,30 @@
                                         </el-select>
                                     </template>
                                     <div v-if="it.button" style="display:inline-block">
-                                        <el-button v-if="it.button.show">{{ it.button.text }}</el-button>
+                                        <el-button v-if="it.button.show" :disabled="it.disabled || publicDisabled">{{ it.button.text }}</el-button>
                                     </div>
                                     <!-- <el-input type="textarea" v-model="ruleForm.desc"></el-input> -->
-                                    <div class="selectTextarea">
+                                    <!-- <div class="selectTextarea">
                                         <el-input v-if="it.textarea && it.textarea.show" type="textarea" v-model="instructions" :disabled="it.disabled"></el-input>
-                                    </div>
+                                    </div> -->
                                 </el-form-item>
+                                <div class="selectTextarea">
+                                    <el-input v-if="it.textarea && it.textarea.show" type="textarea" v-model="instructions" :disabled="it.disabled || publicDisabled"></el-input>
+                                </div>
                             </el-col>
                             <el-col v-else-if="it.type == 'div'" :span="it.span || 12" :key="itIndex">
-                                <el-form-item :key="itIndex">
+                                <el-form-item :key="itIndex" class="foot_operation">
                                     <div style="display:inline-block;">
                                         <el-button @click="upMessage">上一条</el-button>
                                         <el-button @click="nextMessage">下一条</el-button>
                                     </div>
                                     <div class="checkClass">
                                         <person :dptUserConfig.sync="dptUserConfig" v-on:instructionHandler="instructionHandler"></person>
-                                        <el-checkbox v-model="releasePeople" @change="checkboxChange" :checked="checked" ref="peopleCheckbox">指定下达人员</el-checkbox>
+                                        <span>
+                                            <input type="checkbox" :checked="checked" @click="customCheckbox" :disabled="it.checkbox.disabled || publicDisabled">
+                                            <span>指定下达人员</span>
+                                        </span>
+                                        <!-- <el-checkbox v-model="releasePeople" @change="checkboxChange" :checked="checked" ref="peopleCheckbox">指定下达人员</el-checkbox> -->
                                         <div class="footDiv">批示下达</div>
                                     </div>
                                 </el-form-item>
@@ -84,11 +92,25 @@ export default {
     },
     data() {
         return {
+            publicDisabled:undefined,
+            selectDsiabled:undefined,//选择框不可编辑。
             releasePeople:"",
             instructions:"",
             dptUserConfig:{},
             selectValue:"",
+            selectValues:[],
             checked:false//是否选中
+        }
+    },
+    watch: {
+        formConfig (newValue,oldValue) {
+            debugger;
+            let me = this,rowData = newValue.rowData;
+            me.publicDisabled = undefined;
+            me.selectDsiabled - undefined;
+            if(rowData){
+                me.copingStrategies(rowData);
+            }
         }
     },
     /**
@@ -96,11 +118,6 @@ export default {
      */
     created() {
         let me = this;
-        // this.axios.get("/cnbi/json/source/tjsp/szcJson/risk/basicsModalConfig.json").then(res => {
-        //     if(res.data.code == 200) {
-        //         me.formConfig = res.data.formConfig;
-        //     }
-        // });
     },
     /**
      * 页面加载完成后的回调。
@@ -156,12 +173,13 @@ export default {
         setDefaultValue(){
             debugger;
             let me = this;
-            me.selectValue = "";
+            me.selectValues = [];
             me.dptUserConfig = {
                 id:"dptUser",
                 show:false,
                 userDatas:[]
             };
+            me.checked = false;
             // me.$refs.peopleCheckbox[0].isChecked = false;
             // let classValue = me.$refs.peopleCheckbox[0].$el.getAttribute("class");
             // let spanClassValue = me.$refs.peopleCheckbox[0].$el.childNodes[0].getAttribute("class");
@@ -173,6 +191,7 @@ export default {
             // let myEvent = new Event('click');
             // me.$refs.peopleCheckbox[0].$el.dispatchEvent(myEvent);
             // me.$refs.peopleCheckbox[0].$el.removeAttribute("is-checked");
+            // me.$refs.peopleCheckbox[0].$el.childNodes[0].childNodes[1].checked = true;
         },
         /**
          * 下拉框出现隐藏的回调。
@@ -182,6 +201,49 @@ export default {
             let me = this;
             if(item){
 
+            }
+        },
+        /**
+         * 自定义的复选框事件。
+         * @author szc 2019年5月23日10:14:14
+         */
+        customCheckbox (event) {
+            debugger;
+            let me = this,storeParams = me.$store.getters,
+                company = storeParams.company;
+            //判断是不是选择了应对策略。
+            if(me.selectValues && me.selectValues.length == 0){
+                me.checked = false;
+                event.currentTarget.checked = me.checked;
+                me.$message({
+                    message:"请选择风险应对策略",
+                    type:"warning"
+                });
+                return;
+            }
+            if(event.currentTarget.checked){
+                let params = {
+                    company:company
+                };
+                me.checked = event.currentTarget.checked;
+                me.axios.get("/cnbi/json/source/tjsp/riskSql/riskControl/sql.json").then(res => {
+                    if(res.data.code == 200){
+                        params = riskPublic.paramsOfSql(params,res.data.sqlList,"102");
+                        findThirdPartData(params).then(res => {
+                            debugger;
+                            if(res.data.code == 200) {
+                                me.parseTreeData(me.dptUserConfig,res.data.data);
+                            }
+                        });
+                    }
+                });
+            }else {
+                me.checked = event.currentTarget.checked;
+                me.dptUserConfig = {
+                    id:"dptUser",
+                    show:false,
+                    userDatas:[]
+                };
             }
         },
         /**
@@ -274,26 +336,45 @@ export default {
         instructionHandler (nodes) {
             debugger;
             let me = this,storeParams = me.$store.getters,
-            company = storeParams.company;
+            company = storeParams.company,user = storeParams.user.user.userName;
             if(nodes && nodes.length > 0){
                 let arrUser = [],userStr = "";
                 nodes.forEach(item => {
                     arrUser.push(item.id);
                 });
+                // let selectStr = me.selectValues.join(',');
+                let selectStr = me.selectValues[0];
                 userStr = arrUser.join(',');
                 let params = [
                     {
+                        id: 0,
                         company:company,
-                        departId:me.formConfig.departId,
-                        riskYDCL:me.selectValue,
-                        riskContent:me.instructions,
-                        userStr:userStr
+                        nrelateid: me.formConfig.rowData.id,
+                        sinstructionsuser:user,
+                        nstrategy:selectStr,
+                        period: me.parsePeriod(),
+                        sinstructscontent:me.instructions,
+                        sisinstructions:"1"
                     }
                 ];
                 updateInstruction(params).then(res => {
                     debugger;
                     if(res.data.code = 200) {
-
+                        me.checked = false;
+                        me.selectDsiabled = "disabled";
+                        me.publicDisabled = "disabled";
+                        me.dptUserConfig.show = false;
+                        let params = {
+                            id:"10401",
+                            text:"批示之后"
+                        };
+                        me.$emit("eventHandler",params);
+                        me.$message({
+                            message:"批示成功！",
+                            type:"success"
+                        });
+                    }else {
+                        me.$message.errer("批示出错！");
                     }
                 });
             }
@@ -302,13 +383,68 @@ export default {
             //     data:nodes
             // };
             // me.$emit("eventHandler",params);
+        },
+        /**
+         * 转换日期。
+         * @author szc 2019年5月22日19:04:24
+         */
+        parsePeriod(){
+            let me = this,storeParams = me.$store.getters,
+            year = storeParams.year,month = storeParams.month,period = "";
+            if(month > 9) {
+                period = year + "" + month;
+            }else {
+                period = year + "0" + month;
+            }
+            return period;
+        },
+        /**
+         * 恢复默认值。
+         * @author szc 2019年5月23日14:07:30
+         */
+        recoveryDefault(){
+            debugger;
+            let me = this;
+            me.checked = false;
+            me.dptUserConfig.show = false;
+            // me.dptUserConfig = {
+            //     id:"dptUser",
+            //     show:false,
+            //     userDatas:[]
+            // };
+        },
+        /**
+         * 应对策略选择，附上去
+         * @author szc 2019年5月23日21:02:00
+         */
+        copingStrategies (rowData) {
+            debugger;
+            let me = this;
+            me.selectValues = [rowData.fxydcl];
         }
     },
 }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
+.basics_modal {
     .selectTextarea {
-        margin-top: 5px;
+        margin-top: -15px;
+        margin-left: 100px;
+    }
+    // 选择框的内容flex样式
+    .label_select_content {
+        display: flex;
+        // 自定义label的样式
+        .el-form-item__label {
+            align-self: center !important;
+        }
+        .el-form-item__content {
+            margin-left: 0px !important;
+        }
+    }
+    
+    .foot_operation {
+        margin-top: 20px;
     }
     .checkClass {
         display: inline-block;
@@ -353,5 +489,7 @@ export default {
         line-height: 30px;
         text-align: center;
     }
+}
+    
 </style>
 
