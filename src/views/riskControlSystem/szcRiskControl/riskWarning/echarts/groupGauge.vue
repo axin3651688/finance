f0b90f8e64e8eb1f1faac43a822e2da67a9e5146
szc <szc@cnbisoft.com>
2019-06-04 08:44:23 +0800

    提交

---
+++ src/views/riskControlSystem/szcRiskControl/riskWarning/echarts/groupGauge.vue
@@ -0,0 +1,28 @@
+<template>
+    <chart :options.sync="receive" auto-resize/>
+</template>
+<script>
+    export default {
+        props: {},
+        data() {
+            return {
+                receive : {
+                    tooltip : {
+                        formatter: "{a} <br/>{b} : {c}%"
+                    },
+                    series: [
+                        {
+                            name: '业务指标',
+                            type: 'gauge',
+                            detail: {formatter:'{value}%'},
+                            data: [{value: 50, name: '完成率'}]
+                        }
+                    ]
+                }
+            };
+        },
+        mounted() {},
+        watch: {},
+        methods: {}
+    };
+</script>
