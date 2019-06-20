67330e13d87d113a387c360d2cb50c2bb446c1e0
gongjiaxin <axin3651688@aliyun.com>
2019-06-20 14:59:49 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -75,7 +75,7 @@ let proxyObj = { // 配置跨域
 };
 module.exports = {
   // baseUrl: debug ? '/' : '/production-sub-path/',
-  baseUrl: debug ? '/' : '/jsnk',//attendance.. client
+  baseUrl: debug ? '/' : '/finance',//attendance.. client
   outputDir: 'dist', // 构建输出目录
   assetsDir: 'assets', // 静态资源目录 (js, css, img, fonts)
   lintOnSave: false, // 是否开启eslint保存检测，有效值：ture | false | 'error'
