860392a37f191a514a5b78f05acd5449157639c0
szc <szc@cnbisoft.com>
2019-07-02 08:39:57 +0800

    提交

--- src/views/intelligenceReport/progressStatistics.vue
+++ src/views/intelligenceReport/progressStatistics.vue
@@ -7,6 +7,7 @@
 <script>
 import headerTable from "./../riskControlSystem/szcRiskControl/riskWarning/riskTable/threeHeaderTable"
 import { mapGetters,mapActions } from "vuex";
+import { queryTableCensus } from "~api/fill"
 export default {
     name:"progressStatistics",
     components:{
@@ -21,21 +22,23 @@ export default {
                     text:"审阅",
                     children:[
                         {
-                            id:"",
-                            text:"操作人"
+                            id:"sreviewuser",
+                            text:"操作人",
+                            align:"center"
                         },
                         {
-                            id:"",
-                            text:"状态"
+                            id:"sreview",
+                            text:"状态",
+                            align:"center"
                         }
                     ]
                 },
                 {
-                    id:"",
+                    id:"companyname",
                     text:"公司名称"
                 },
                 {
-                    id:"",
+                    id:"templatename",
                     text:"报表名称"
                 },
                 {
@@ -47,40 +50,46 @@ export default {
                     text:"抽取",
                     children:[
                         {
-                            id:"",
-                            text:"操作人"
+                            id:"sextractuser",
+                            text:"操作人",
+                            align:"center"
                         },
                         {
-                            id:"",
-                            text:"状态"
+                            id:"sextract",
+                            text:"状态",
+                            align:"center"
                         }
                     ]
                 },
                 {
-                    id:"",
+                    id:"tb",
                     text:"填报",
                     children:[
                         {
-                            id:"",
-                            text:"操作人"
+                            id:"sfilluser",
+                            text:"操作人",
+                            align:"center"
                         },
                         {
-                            id:"",
-                            text:"状态"
+                            id:"sfill",
+                            text:"状态",
+                            align:"center"
                         }
                     ]
                 },
                 {
-                    id:"",
+                    id:"sb",
                     text:"上报",
                     children:[
                         {
-                            id:"",
-                            text:"操作人"
+                            id:"sreportuser",
+                            text:"操作人",
+                            align:"center"
                         },
                         {
-                            id:"",
-                            text:"状态"
+                            id:"sreport",
+                            text:"状态",
+                            align:"center"
                         }
                     ]
                 }
@@ -96,12 +105,15 @@ export default {
          */
         company(newValue, oldValue) {
             let me = this;
+            this.updateData();
         },
         year(newValue, oldValue) {
             let me = this;
+            this.updateData();
         },
         month(newValue, oldValue) {
             let me = this;
+            this.updateData();
         }
     },
     created() {
@@ -112,9 +124,29 @@ export default {
             day:false,
             conversion:false
         });
+        this.updateData();
     },
     methods: {
         ...mapActions(["ShowDims"]),
+        updateData(){
+            debugger;
+            let me = this,storeParams = me.$store.getters,company = storeParams.company,
+                year = storeParams.year,month = storeParams.month,period = "";
+            if(month > 9){
+                period = year + "" + month;
+            }else {
+                period = year + "0" + month;
+            }
+            let params = {
+                company:company,
+                period:period
+            };
+            queryTableCensus(params).then(res => {
+                if(res.data.code == 200){
+                    me.tableData = res.data.data;
+                }
+            });
+        }
     },
     mounted() {
         
