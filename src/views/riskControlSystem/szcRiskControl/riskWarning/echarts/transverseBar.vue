9be71626cd7f89ac858b62920a3b8d5d09118bdb
szc <szc@cnbisoft.com>
2019-07-23 11:46:29 +0800

    提交

--- src/views/riskControlSystem/szcRiskControl/riskWarning/echarts/transverseBar.vue
+++ src/views/riskControlSystem/szcRiskControl/riskWarning/echarts/transverseBar.vue
@@ -21,12 +21,9 @@ export default {
                         type: 'shadow'
                     }
                 },
-                // legend: {
-                //     // data: ['2011年', '2012年']
-                // },
                 grid: {
                     left: '3%',
-                    right: '4%',
+                    right: '13%',
                     // bottom: '6%',
                     containLabel: true
                 },
@@ -42,29 +39,16 @@ export default {
                     {
                         type: 'slider',
                         show: true,
-                        startValue:10,
-                        endValue:16,
-                        handleSize: 10,
+                        startValue:this.sValue,
+                        endValue:this.eValue,
+                        handleSize: 0,
                         yAxisIndex:0,
-                        orient:'horizontal',
-                    },
-                    // {
-                    //     type: 'inside',
-                    //     start: 94,
-                    //     end: 100,
-                    //     orient:'horizontal'
-                    // },
-                    // {
-                    //     type: 'slider',
-                    //     show: true,
-                    //     yAxisIndex: 0,
-                    //     filterMode: 'empty',
-                    //     width: 12,
-                    //     height: '70%',
-                    //     handleSize: 8,
-                    //     showDataShadow: false,
-                    //     left: '93%'
-                    // }
+                        orient:'vertical',
+                        right:'3%',
+                        showDataShadow: false,
+                        showDetail:false,
+                        moveOnMouseWheel:true
+                    }
                 ],
                 series: [
                     {
@@ -73,24 +57,21 @@ export default {
                         barWidth:'50',
                         itemStyle: {   
                             //通常情况下：
-                            normal:{  
-            　　　　　　　　　　　　//每个柱子的颜色即为colorList数组里的每一项，如果柱子数目多于colorList的长度，则柱子颜色循环使用该数组
-                                color: function (params){
-                                    var colorList = ['rgb(164,205,238)','rgb(42,170,227)','rgb(25,46,94)','rgb(195,229,235)'];
-                                    return colorList[params.dataIndex];
-                                }
-                            },
-                            //鼠标悬停时：
-                            // emphasis: {
-                            //         shadowBlur: 10,
-                            //         shadowOffsetX: 0,
-                            //         shadowColor: 'rgba(0, 0, 0, 0.5)'
-                            // }
+                            normal:{
+                                color:'#c3e5eb'  
+            　　　　　　　　　　　//每个柱子的颜色即为colorList数组里的每一项，如果柱子数目多于colorList的长度，则柱子颜色循环使用该数组
+                                // color: function (params){
+                                //     var colorList = ['rgb(164,205,238)','rgb(42,170,227)','rgb(25,46,94)','rgb(195,229,235)'];
+                                //     return colorList[params.dataIndex];
+                                // }
+                            }
                         },
                         data: []
                     }
                 ]
-            }
+            },
+            sValue:0,
+            eValue:9,
         }
     },
     computed: {
@@ -111,12 +92,13 @@ export default {
         },
         year(newValue, oldValue) {
             this.receive.series[0].name = newValue + "年";
+            this.updateData();
         }
     },
     methods: {
         updateData(){
             let me = this;
-            if(me.chartData){
+            if(me.chartData && me.chartData.length > 0){
                 let datas = me.chartData,yDatas = [],seriesData = [];
                 // datas.forEach(item => {
                 //     if(item.score){
@@ -128,12 +110,21 @@ export default {
                     return item.score;
                 });
                 if(arrDatas && arrDatas.length > 0){
-                    me.sortByProp(arrDatas,"score",-1);
+                    me.sortByProp(arrDatas,"score",1);
                 }
                 arrDatas.forEach(item => {
                     yDatas.push(item.sname);
                     seriesData.push(item.score);
                 });
+                //滚动条的位置。
+                me.receive.dataZoom[0].startValue = arrDatas.length - 1;
+                if(arrDatas.length > 10){
+                    me.receive.dataZoom[0].endValue = arrDatas.length - 10;
+                }else {
+                    me.receive.dataZoom[0].endValue = 0;
+                }
+                // me.sValue = arrDatas.length - 1;
+                // me.eValue = arrDatas.length - 10;
                 me.receive.yAxis.data = yDatas.reverse();
                 me.receive.series[0].data = seriesData.reverse();
             }
@@ -144,9 +135,9 @@ export default {
         sortByProp (data,prop,num) {
             data.sort((a,b) => {
                 if(a[prop] > b[prop]){
-                    return num;
-                }else if(a[prop] < b[prop]) {
                     return -num;
+                }else if(a[prop] < b[prop]) {
+                    return num;
                 }else {
                     return 0;
                 }
